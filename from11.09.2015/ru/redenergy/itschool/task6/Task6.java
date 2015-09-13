b5c3888975ae49bcf85592a3405379edf249db6d
RedEnergy <forwot163@gmail.com>
2015-09-13 20:10:24 +0400

    Cyrillic replaces with unicode

--- from11.09.2015/ru/redenergy/itschool/task6/Task6.java
+++ from11.09.2015/ru/redenergy/itschool/task6/Task6.java
@@ -6,7 +6,7 @@ public class Task6 {
 		try{
 			int result = 10 / 0;
 		} catch (ArithmeticException ex){
-			System.out.println("На нуль делить нельзя");
+			System.out.println("\u041d\u0430 \u043d\u0443\u043b\u044c \u0434\u0435\u043b\u0438\u0442\u044c \u043d\u0435\u043b\u044c\u0437\u044f");
 		}
 	}
 
